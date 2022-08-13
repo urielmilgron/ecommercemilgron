48ae7f2255d6f0f563836161aeaa48399c1d71d8
Uriel Milgron <urielmilgron18@gmail.com>
2022-08-13 16:44:33 -0300

    CounterAdded

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
-import Counter from './Components/Counter/Counter'
+
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  // const handleOnAdd = (quantity)=>{
-  //   console.log(`Se agregaron ${quantity} items`)
-  // }
+
 
   return (
     <div className="App">
@@ -22,7 +19,6 @@ function App() {
         <Route path="/category/:categoryId" element={<ItemListContainer greeting='Filtrado'/>} />
         <Route path="/item/:productId" element={<ItemDetailContainer/>}/>
         <Route path='*' element={<h1>404 NOT FOUND</h1>} /> 
-      {/* <Counter stock={10} onAdd={handleOnAdd}/> */}
       </Routes>
       </BrowserRouter>
     </div>
